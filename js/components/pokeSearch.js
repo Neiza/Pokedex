8b35e73580aa7de1e5551991af0f1d2623b54509
Neiza Nunez <neizapf@gmail.com>
2017-06-16 15:15:16 -0500

    filter

--- js/components/pokeSearch.js
+++ js/components/pokeSearch.js
@@ -1,5 +1,5 @@
 "use strict";
-const PokemonItem = (pokemon, update) => {
+const PokemonItem = (pokemon) => {
   const pokeItem = $('<div class="poke-item"></div>');
   const pokeImg = $('<img src= "http://serebii.net/art/th/"'+ pokemon.pokemon_entries.entry_number +'".png"></img>');
   const pokeIcons = $('<div class="poke-icons"></span>');
@@ -7,12 +7,11 @@ const PokemonItem = (pokemon, update) => {
   const pokeHeart = $('<span class="poke-heart"></span>');
   const pokeArrows = $('<span class="poke-arrows"></span>');
   const pokeName = $('<h4>'+ pokemon.pokemon_entries.pokemon_species.name +'</h4>');
-
+console.log(pokemons);
   pokeball.on('click',(e) => {
     e.preventDefault();
-    alert("elegiste un pokemon"); // aqui hay que poner la funcion del modal
-    //state.selectedStation = station;
-    update();
+     // aqui hay que poner la funcion del modal
+    state.selectedPokemon = pokemon;
   });
 
   pokeIcons.append(pokeball);
@@ -25,12 +24,12 @@ const PokemonItem = (pokemon, update) => {
   return pokeItem;
 }
 
-const reRender = (container, filter, update) => {
+const reRender = (container, filter) => {
   container.empty();
-  const filteredStations = filterByName(state.pokemons,filter);
-  if (filteredStations.length > 0) {
-    $.each(filteredStations,(index,pokemon) => {
-      container.append(PokemonItem(pokemon,update));
+  const filteredPokemons = filterByName(state.pokemons,filter);
+  if (filteredPokemons.length > 0) {
+    $.each(filteredPokemons,(index,pokemon) => {
+      container.append(PokemonItem(pokemon));
     });
   } else {
     container.append($('<p>Pokemon no encontrado</p>'));
@@ -41,7 +40,7 @@ const PokeSearch =  () => {
   const searchContainer = $('<div class="search-container"></div>');
   const searchInput = $('<input type="text" class="search-input">');
   const searchResult = $('<div class="search-result"></div>');
-
+console.log(state.pokemons);
   searchContainer.append(searchInput);
   searchContainer.append(searchResult);
 
@@ -49,7 +48,5 @@ const PokeSearch =  () => {
     const filter = searchInput.val();
     reRender(searchResult,filter);
   });
-  reRender(searchResult,"");
-
   return searchContainer;
 }
